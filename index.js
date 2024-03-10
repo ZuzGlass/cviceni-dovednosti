02443479c0dbe61607c0526c2ee1f624ec48d5e2
Zuzana Brýlová <zuzana.brylova@gmail.com>
2024-03-10 17:18:46 +0100

    Finalni verze.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log('funguju!');
+
+const updateSkill = (id, number) => {
+        const posuvnikPosun = document.querySelector(`#${id} .skill__progress`)
+        posuvnikPosun.style.width = `${number}%`
+    
+        const posuvnikHodnota = document.querySelector(`#${id} .skill__value`)
+        posuvnikHodnota.textContent = `${number} / 100`
+
+        return updateSkill
+    
+     }
+    
+const html = Number(prompt("Zadej svou úroveň dovedností v HTML jako číslo v rozmezí 0-100:"))
+const css = Number(prompt("Zadej svou úroveň dovedností v CSS jako číslo v rozmezí 0-100:"))
+const js = Number(prompt("Zadej svou úroveň dovedností v JavaScript jako číslo v rozmezí 0-100:"))
+    
+updateSkill("skill1", html)
+updateSkill("skill2", css)
+updateSkill("skill3", js)
